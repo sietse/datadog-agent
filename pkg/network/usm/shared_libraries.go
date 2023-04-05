7ed5b4257d4e4c5e98b942b95293159a298a97a4
Nicolas PLANEL <nicolas.planel@datadoghq.com>
2023-04-05 13:09:28 +0200

    make it work

--- pkg/network/usm/shared_libraries.go
+++ pkg/network/usm/shared_libraries.go
@@ -15,8 +15,10 @@ import (
 	"go.uber.org/atomic"
 	"os"
 	"regexp"
+	"strings"
 	"sync"
 	"syscall"
+	"time"
 	"unsafe"
 
 	"github.com/DataDog/gopsutil/process"
@@ -316,7 +318,40 @@ func (r *soRegistry) unregister(pid uint32) {
 	delete(r.byPID, pid)
 }
 
-// register a ELF library root/libPath as be used by the pid
+func quircksWaitLib(hostlibPath string) error {
+	// java netty when injecting libnetty_tcnative_linux_.*.so would copy the file (from his jar) to a workdir
+	// then inject it to the process via System.Loadlibrary()
+	// usually we got the event when the file is just created and contain 0 bytes
+	if strings.Contains(hostlibPath, "libnetty_tcnative_linux_") {
+		s, err := os.Stat(hostlibPath)
+		if err != nil {
+			return err
+		}
+		time.Sleep(time.Millisecond)
+		libSize := s.Size()
+		sizeOK := false
+		end := time.Now().Add(100 * time.Millisecond)
+		for !time.Now().After(end) {
+			s, err = os.Stat(hostlibPath)
+			if err != nil {
+				return err
+			}
+			if s.Size() != libSize || s.Size() == 0 {
+				libSize = s.Size()
+				time.Sleep(time.Millisecond)
+				continue
+			}
+			sizeOK = true
+			break
+		}
+		if !sizeOK {
+			return fmt.Errorf("timeout current size %d", libSize)
+		}
+	}
+	return nil
+}
+
+// Register a ELF library root/libPath as be used by the pid
 // Only one registration will be done per ELF (system wide)
 func (r *soRegistry) register(root, libPath string, pid uint32, rule soRule) {
 	hostLibPath := root + libPath
@@ -343,6 +378,11 @@ func (r *soRegistry) register(root, libPath string, pid uint32, rule soRule) {
 			}
 			r.byPID[pid][pathID] = struct{}{}
 		}
+		return
+	}
+
+	if err := quircksWaitLib(hostLibPath); err != nil {
+		log.Debugf("error registering library during waiting for lib %s path %s by pid %d : %s", pathID.String(), hostLibPath, pid, err)
 		return
 	}
 
